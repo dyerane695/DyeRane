0f67057a15294df98fd0e0a04c9487477f235371
dyerane695 <dyerane695@gmail.com>
2026-09-23 12:45:48 +0545

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,28 @@
+
+function goToPage() {
+    window.location.href = "gallery.html";
+}
+
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+function showPopup2() {
+    document.getElementById("popup2").style.display = "block";
+}
+
+function closePopup2() {
+    document.getElementById("popup2").style.display = "none";
+}
+function goToPage2() {
+    window.location.href = "https://www.youtube.com/@dyerane695";
+}
+function goToPage3() {
+    window.location.href = "https://www.tiktok.com/@dyerane695";
+}
+function goToPage4() {
+    window.location.href = "https://www.facebook.com/@DyeRane695";
+}
